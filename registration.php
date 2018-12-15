ea4be56a85161ed6ddf3ae62662a87e71d02e80c
ThirdAstronaut <lechowicz.rafal@gmail.com>
2018-12-15 02:14:23 +0100

    cannot connect to db yet

---
+++ registration.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset = "utf-8">
+      <title>Registration Form</title>
+      <style type = "text/css">
+         p       { margin: 0px; }
+         .error  { color: red }
+         p.head  { font-weight: bold; margin-top: 10px; }
+         label   { width: 5em; float: left; }
+      </style>
+   </head>
+   <body>
+      <?php
+         $login = isset($_POST[ "login" ]) ? $_POST[ "login" ] : "";
+         $passwd = isset($_POST[ "passwd" ]) ? $_POST[ "passwd" ] : "";   
+         $fname = isset($_POST[ "fname" ]) ? $_POST[ "fname" ] : "";
+         $lname = isset($_POST[ "lname" ]) ? $_POST[ "lname" ] : "";
+         $email = isset($_POST[ "email" ]) ? $_POST[ "email" ] : "";
+         $phone = isset($_POST[ "phone" ]) ? $_POST[ "phone" ] : "";
+   
+         $iserror = false;
+         $formerrors = 
+            array( "loginerror" => false,"passwderror" => false,"fnameerror" => false, "lnameerror" => false, 
+               "emailerror" => false, "phoneerror" => false );
+
+         $inputlist = array(  "login" => "Login", "passwd" => "Password", "fname" => "First Name",
+            "lname" => "Last Name", "email" => "Email",
+            "phone" => "Phone" );
+
+         if ( isset( $_POST["submit"] ) )
+         {
+            if ( $login == "" )                   
+            {
+               $formerrors[ "loginerror" ] = true;
+               $iserror = true;                   
+            } // end if
+            if ( $passwd == "" )                   
+            {
+               $formerrors[ "passwderror" ] = true;
+               $iserror = true;                   
+            } // end if
+
+            if ( $fname == "" )                   
+            {
+               $formerrors[ "fnameerror" ] = true;
+               $iserror = true;                   
+            } // end if
+
+            if ( $lname == "" ) 
+            {
+               $formerrors[ "lnameerror" ] = true;
+               $iserror = true;
+            } // end if
+
+            if ( $email == "" ) 
+            {
+               $formerrors[ "emailerror" ] = true;
+               $iserror = true;
+            } // end if       
+
+            if ( !preg_match( '/^[0-9]{9,11}$/', 
+               $phone ) ) 
+            {
+               $formerrors[ "phoneerror" ] = true;
+               $iserror = true;
+            } // end if
+            $link = mysqli_connect( "localhost", 
+                  "root", "root" );
+            if ( !$iserror )  
+            {
+               $query = "INSERT INTO users " .
+                  "( Login, Password, LastName, FirstName, Email, Phone) " .
+                  "VALUES ( '$login','$passwd','$lname', '$fname', '$email', 
+" . "'" . mysqli_real_escape_string( $link, $phone ) . ")";
+
+               // Connect to MySQL
+               if ( !( $database = mysqli_connect( "localhost", 
+                  "root", "root" ) ) )
+                  die( "<p>Could not connect to database</p>" );
+
+               mysqli_close( $database );
+
+               print( "<p class = 'head'>The following information has been 
+                     saved in our database:</p>
+                  <p>Login: $login</p>
+                  <p>Password: $passwd</p>
+                  <p>Name: $fname $lname</p>
+                  <p>Email: $email</p>
+                  <p>Phone: $phone</p>
+                  <p><a href = 'formDatabase.php'>Click here to view 
+                     entire database.</a></p>
+                  </body></html>" );
+               die(); // finish the page
+            } // end if 
+         } // end if 
+
+         print( "<h1>Registration Form</h1>");
+            if ( $iserror )                                              
+         {                                                            
+            print( "<p class = 'error'>Fields with * need to be filled 
+               in properly.</p>" );
+         } // end if
+
+         print( "<!-- post form data to dynamicForm.php -->
+            <form method = 'post' action = 'registration.php'>
+            <!-- create four text boxes for user input -->" );
+         foreach ( $inputlist as $inputname => $inputalt )
+         {
+            print( "<div><label>$inputalt:</label><input type = 'text'
+               name = '$inputname' value = '" . $$inputname . "'>" );
+            
+            if ( $formerrors[ ( $inputname )."error" ] == true ) 
+               print( "<span class = 'error'>*</span>" );        
+            
+            print( "</div>" );
+         } // end foreach
+
+         if ( $formerrors[ "phoneerror" ] ) 
+            print( "<p class = 'error'>Must be in the form 
+               +4812345689" );
+          
+         $counter = 0;
+        
+         print( "<!-- create a submit button -->
+            <p class = 'head'><input type = 'submit' name = 'submit'
+            value = 'Register'></p></form></body></html>" );
+   ?><!-- end PHP script -->
